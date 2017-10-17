356c1c2a893d6bb64d906db93846a3f521886a11
chatopadhyaye <shadanahmad76@gmail.com>
2017-10-17 20:44:32 +0530

    my first ever commit

---
+++ piper/urls.py
@@ -0,0 +1,32 @@
+"""piper URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url
+from django.contrib import admin
+from piper import views
+from books.views import search, contact
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^hello/$', views.hello),
+    url(r'^time/$', views.current_datetime, name = "date"),
+    url(r'^time/plus/(\d{1,2})/$', views.hours_ahead),
+    url(r'^pikachu/$', views.display_meta),
+    url(r'^search/$', search), 
+    url(r'^contact/$', contact), 
+    url(r'^public/$', views.public, name = "public"),
+
+
+]
---
+++ piper/views.py
@@ -0,0 +1,36 @@
+from django.template.loader import get_template
+from django.template import Context
+from django.http import Http404, HttpResponse
+from django.shortcuts import render
+import datetime
+
+def hello(request):
+	return HttpResponse ("Hello World")
+
+def current_datetime(request): 
+    now = datetime.datetime.now() 
+    t = get_template('current_datetime.html') 
+    html = t.render({'current_date': now}) 
+    return HttpResponse(html) 
+
+def hours_ahead(request, offset):
+	try:
+		offset = int(offset)
+	except ValueError:
+		raise Http404()
+	dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
+	t = get_template('hours_ahead.html')
+	html = t.render({'hour_offset': offset, 'next_time': dt})
+	return HttpResponse(html)
+
+def display_meta(request):
+    values = list(request.META.items())
+    values.sort()
+    html = []
+    for k, v in values:
+      html.append('<tr><td>%s</td><td>%s</td></tr>' % (k, v))
+    return HttpResponse('<table>%s</table>' % '\n'.join(html))
+
+def public(request):
+    return render(request, 'public.html', {"user_name": "Clark Kent"})
+
